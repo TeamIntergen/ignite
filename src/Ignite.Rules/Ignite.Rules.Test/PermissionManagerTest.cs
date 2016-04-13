3b1c2c1dfa83c02acb7f3a3c3b639d9f74508710
davidsh@intergen.co.nz <davidsh@intergen.co.nz>
2016-04-13 14:19:13 +1200

    add in test across permission sets to load can access.

---
+++ src/Ignite.Rules/Ignite.Rules.Test/PermissionManagerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ignite.Rules.Test
+{
+    [TestFixture]
+    public class PermissionManagerTest
+    {
+        private PermissionManager _permissionManager;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var webLoader = new WebLoader();
+            var settings = new SettingsStub();
+            var repos = new SecurityRepository(webLoader, settings);
+            var useraccessDto = repos.LoadAccessLevel().Result;
+            _permissionManager = new PermissionManager(useraccessDto);
+        }
+
+
+        [Test]
+        [TestCase("non existanct", false, ExpectedException = typeof(InvalidOperationException))]
+        [TestCase("empty", false, ExpectedException = typeof(InvalidOperationException))]
+        [TestCase(null, false, ExpectedException = typeof(ArgumentException))]
+        [TestCase("", false, ExpectedException = typeof(ArgumentException))]
+        [TestCase("Anonymous", false)]
+        [TestCase("AuthenticatedNonAttendee", false)]
+        [TestCase("AttendeeExternalCustomerAndPartner", true)]
+        [TestCase("AttendeeSponsor", true)]
+        [TestCase("AttendeeExhibitor", true)]
+        [TestCase("AttendeeMicrosoft", true)]
+        [TestCase("AttendeeStudent", true)]
+        [TestCase("AttendeeFaculty", true)]
+        [TestCase("AttendeeVIP", true)]
+        [TestCase("AttendeeVIPMicrosoft", true)]
+        [TestCase("ExternalCustomerAndPartner", true)]
+        [TestCase("Microsoft", true)]
+        [TestCase("Microsoft", true)]
+        [TestCase("Sponsor", true)]
+        [TestCase("Exhibitor", true)]
+        [TestCase("SpeakerExternalCustomerPartnerHOTEL", true)]
+        [TestCase("SpeakerExternalCustomerPartnerNOHOTEL", true)]
+        [TestCase("SpeakerandExpertMicrosoftNOHOTEL", true)]
+        [TestCase("SpeakerMicrosoftNOHOTEL", true)]
+        [TestCase("Press", true)]
+        [TestCase("Crew", true)]
+        [TestCase("ExpoOnly", true)]
+        [TestCase("DayPassAttendee", true)]
+        public void CanAccessMyIgnite(string userType, bool expected)
+        {
+            Assert.That(_permissionManager.CanAccessIgnite(userType),  Is.EqualTo(expected));
+
+            if (userType != null)
+            {
+                Assert.That(_permissionManager.CanAccessIgnite(userType.ToLower()), Is.EqualTo(expected));
+                Assert.That(_permissionManager.CanAccessIgnite(userType.ToUpper()), Is.EqualTo(expected));
+            }
+        }
+    }
+}
---
+++ src/Ignite.Rules/Ignite.Rules/PermissionManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Ignite.Rules.Dto;
+
+namespace Ignite.Rules
+{
+    public class PermissionManager
+    {
+        private readonly Lazy<Dictionary<string, ProfileDto>> _permission;
+
+        public PermissionManager(UseraccessDto access)
+        {
+            _permission = new Lazy<Dictionary<string, ProfileDto>>(() => Build(access));
+        }
+
+        private static Dictionary<string, ProfileDto> Build(UseraccessDto access)
+        {
+            var dict = new Dictionary<string, ProfileDto>
+            {
+                { nameof(access.NonRegistered.Anonymous).ToLower(),  access.NonRegistered.Anonymous},
+                { nameof(access.NonRegistered.AuthenticatedNonAttendee).ToLower(),  access.NonRegistered.AuthenticatedNonAttendee},
+                { nameof(access.ConferenceAttendee.AttendeeExhibitor).ToLower(),  access.ConferenceAttendee.AttendeeExhibitor},
+                { nameof(access.ConferenceAttendee.AttendeeExternalCustomerAndPartner).ToLower(),  access.ConferenceAttendee.AttendeeExternalCustomerAndPartner},
+                { nameof(access.ConferenceAttendee.AttendeeFaculty).ToLower(),  access.ConferenceAttendee.AttendeeFaculty},
+                { nameof(access.ConferenceAttendee.AttendeeMicrosoft).ToLower(),  access.ConferenceAttendee.AttendeeMicrosoft},
+                { nameof(access.ConferenceAttendee.AttendeeSponsor).ToLower(),  access.ConferenceAttendee.AttendeeSponsor},
+                { nameof(access.ConferenceAttendee.AttendeeStudent).ToLower(),  access.ConferenceAttendee.AttendeeStudent},
+                { nameof(access.ConferenceAttendee.AttendeeVIP).ToLower(),  access.ConferenceAttendee.AttendeeVIP},
+                { nameof(access.ConferenceAttendee.AttendeeVIPMicrosoft).ToLower(),  access.ConferenceAttendee.AttendeeVIPMicrosoft},
+                { nameof(access.Crew.Crew).ToLower(),  access.Crew.Crew},
+                { nameof(access.DayPass.DayPassAttendee).ToLower(),  access.DayPass.DayPassAttendee},
+                { nameof(access.ExpoOnly.ExpoOnly).ToLower(),  access.ExpoOnly.ExpoOnly},
+                { nameof(access.Press.Press).ToLower(),  access.Press.Press},
+                { nameof(access.Speaker.SpeakerExternalCustomerPartnerHOTEL).ToLower(),  access.Speaker.SpeakerExternalCustomerPartnerHOTEL},
+                { nameof(access.Speaker.SpeakerExternalCustomerPartnerNOHOTEL).ToLower(),  access.Speaker.SpeakerExternalCustomerPartnerNOHOTEL},
+                { nameof(access.Speaker.SpeakerMicrosoftNOHOTEL).ToLower(),  access.Speaker.SpeakerMicrosoftNOHOTEL},
+                { nameof(access.Speaker.SpeakerandExpertMicrosoftNOHOTEL).ToLower(),  access.Speaker.SpeakerandExpertMicrosoftNOHOTEL},
+                { nameof(access.Staff.ExternalCustomerAndPartner).ToLower(),  access.Staff.ExternalCustomerAndPartner},
+                { nameof(access.Staff.Microsoft).ToLower(),  access.Staff.Microsoft},
+                { nameof(access.BoothStaff.Exhibitor).ToLower(),  access.BoothStaff.Exhibitor},
+                { nameof(access.BoothStaff.Sponsor).ToLower(),  access.BoothStaff.Sponsor},
+            };
+            return dict;
+        }
+
+        public bool CanAccessIgnite(string userType)
+        {
+            var profile = LookupUserProfile(userType);
+            return profile.Access.MyIgnite;
+        }
+
+        private ProfileDto LookupUserProfile(string userType)
+        {
+            if (string.IsNullOrEmpty(userType))
+            {
+                throw new ArgumentException("Unable to lookup empty user type");
+            }
+
+            var key = userType.ToLower();
+            if (!_permission.Value.ContainsKey(key))
+            {
+                throw new InvalidOperationException($"user type {userType} not found");
+            }
+            return _permission.Value[key];
+        }
+    }
+}
